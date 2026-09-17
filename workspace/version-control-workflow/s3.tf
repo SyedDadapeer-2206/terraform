808bb3b336124b258d57eb5241a1563c5e87a729
SyedDadapeer-2206 <syeddadapeer2206@gmail.com>
2026-09-17 19:11:03 +0530

    Add Terraform configuration for AWS S3 bucket

--- workspace/version-control-workflow/s3.tf
+++ workspace/version-control-workflow/s3.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_s3_bucket" "mybucket" {
   bucket = "terraform-import-demo-202609"
 
