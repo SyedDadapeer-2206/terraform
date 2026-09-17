6400823dc727f62a4adae70f6496886ac99211e5
SyedDadapeer-2206 <syeddadapeer2206@gmail.com>
2026-09-17 19:19:34 +0530

    Rename S3 bucket for version control demo

--- workspace/version-control-workflow/s3.tf
+++ workspace/version-control-workflow/s3.tf
@@ -1,18 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "terraform-import-demo-202609"
+  bucket = "terraform-hcp-vcs-demo-202609"
 
   tags = {
     ManagedBy = "Terraform"
